348d612e440c71cd85176b252c1e3cb6f454e4bd
dhanraj-d <93528725+Dhanraj-d@users.noreply.github.com>
2025-06-17 13:32:56 +0530

    Create ec2-clean_up.sh

---
+++ ec2-clean_up.sh
@@ -0,0 +1,17 @@
+sudo du -sh /var
+sudo du -sh /home
+sudo du -sh /opt
+sudo du -sh /usr
+sudo du -sh /tmp
+sudo du -sh /root
+
+
+
+
+sudo du -sh /home/ubuntu/* | sort -h
+
+
+ du -sh /home/ubuntu/spartan/* | sort -h
+
+
+ : > /home/ubuntu/ultron/flyer-rakshak/nohup.out
